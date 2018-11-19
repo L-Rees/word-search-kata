a0827b79f0f6595416c0a074dec7833c03aa8aaf
Lisa Rees <kortirion@gmail.com>
2018-11-19 18:24:46 -0500

    redo of test one to return the list

---
+++ src/main/java/wordSearch/WordSearch.java
@@ -0,0 +1,18 @@
+package wordSearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WordSearch {
+
+	public ArrayList<String> search(String wordMatrix, String wordList) {
+		ArrayList<String> result = new ArrayList<>();
+		
+		 ArrayList<String> wordListArray= new ArrayList<String>(Arrays.asList(wordList.split(" ")));
+	
+		
+		return wordListArray;
+	}
+
+}
---
+++ src/test/java/wordSearch/WordSearchTest.java
@@ -0,0 +1,80 @@
+package wordSearch;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class WordSearchTest {
+
+	
+	String wordMatrix = "R T L W I L L I A M K T A A G " +
+			"K U S H N G B I H E O H X H R " +
+			"Z O F A L E L A L M O O R B E " +
+			"G E O O G E C R R Q T R U A A " +
+			"E O I T B A O R E D R I B L T " +
+			"B N L O A N D L O E E N M I G " +
+			"F E L L D N V A N M R D O N O " +
+			"L D O L U E N O R I A E B H B " +
+			"A A I R N M I O R K O N T C L " +
+			"D I L K N L S O D A R O C C I " +
+			"N N I L A W D L S A L B I E N " +
+			"A N K G L U B M I F L O G R R " +
+			"G S M A U G B E R T U L N I O " +
+			"R U F I B I L I F B B G E G Q " +
+			"B I L B O B A G G I N S L B G";
+	
+	
+	String wordList = "BALIN " + 
+			"BARD " + 
+			"BELLADONNATOOK " + 
+			"BEORN " + 
+			"BERT " + 
+			"BIFUR " + 
+			"BILBOBAGGINS " + 
+			"BOFUR " + 
+			"BOLG " + 
+			"BOMBUR " + 
+			"BULLROARERTOOK " + 
+			"DAIN " + 
+			"DORI " + 
+			"DWALIN " + 
+			"ELROND " + 
+			"ELVENKING " + 
+			"FILI " + 
+			"GALION " + 
+			"GANDALF " + 
+			"GLOIN " + 
+			"GOLFIMBUL " + 
+			"GOLLUM " + 
+			"GREATGOBLIN " + 
+			"KILI " + 
+			"LOBELIA " + 
+			"LOTHO " + 
+			"NECROMANCER " + 
+			"NORI " + 
+			"OIN " + 
+			"ORI " + 
+			"RADAGAST " + 
+			"SMAUG " + 
+			"THORIN " + 
+			"TOM " + 
+			"WILLIAM";
+	
+
+	@Test
+	public void shouldReturnWords() {
+		WordSearch hobbit = new WordSearch();
+		String shortList = "BALIN BARD SMAUG";
+		ArrayList<String> result = hobbit.search(wordMatrix, shortList);
+		assertThat(result, containsInAnyOrder("BALIN", "SMAUG","BARD"));	
+	}
+	
+	
+	
+	
+}
